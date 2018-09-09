0b82408cbee7703e81b3f14ddc6247800260b4cc
Developer <Developer@DEVELOPER-PC>
2018-09-09 12:25:01 -0500

    [GAP] implememtation of  reposotiry pattern on backend service

---
+++ GapSolution/BackendApplication/Data/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendApplication.Data
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        void Delete(int id);
+        void toUpdate(T entity);
+        int Count(Expression<Func<T, bool>> where);
+        T ObtainById(int id);
+        IEnumerable<T> FindBy(QueryParameters<T> queryParameters);
+    }
+}
